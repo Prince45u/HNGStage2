60b934aac3ac96c325d47bdc6360e9bba37b4c65
Eric Brownson <brownsrikky@gmail.com>
2023-09-17 19:56:10 +0100

    Update Card.js

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -20,9 +20,9 @@ function Card({item}) {
   clip-rule="evenodd"
   d="M1.17157 1.48284C2.73367 -0.0381453 5.26633 -0.0381453 6.82842 1.48284L7.99999 2.62359L9.17157 1.48284C10.7337 -0.0381453 13.2663 -0.0381453 14.8284 1.48284C16.3905 3.00383 16.3905 5.46984 14.8284 6.99083L7.99999 13.6396L1.17157 6.99083C-0.390524 5.46984 -0.390524 3.00383 1.17157 1.48284Z"
   fill={Favorited ? '#e11d48' : '#D1D5DB'}/>
-  </svg></div>
-              <Link className='card-link-container' to={`/movies/${item.id}`}>              
+  </svg></div>              
                 <div data-testid="movie-card"  className="card">
+              <Link className='card-link-container' to={`/movies/${item.id}`}>
                   <div data-testid="movie-poster" className="card-img" style={{ 
                     backgroundImage: `url(https://image.tmdb.org/t/p/w500/${item.poster_path})`,
                     backgroundSize: "cover",
@@ -38,8 +38,8 @@ function Card({item}) {
                       <p className='home-ratings' style={{ color: "#e11d48", fontSize: "14px" }}>{item.vote_average}</p>
                       
                   </div>
-                </div>
               </Link>
+              </div>
     </div>
   )
 }
